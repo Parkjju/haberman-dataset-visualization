4b4fba7eb316d36f5e2287ae38f726ae682bcb00
Parkjju <rudwns3927@gmail.com>
2023-04-28 22:00:25 +0900

    feat: 쿼리파라미터 전달

--- src/api.js
+++ src/api.js
@@ -5,3 +5,15 @@ axios.defaults.baseURL = 'http://localhost:8000';
 export const fetchGraphData = () => {
     return axios.get('/patients/random');
 };
+
+export const fetchPredictedData = ({ queryKey }) => {
+    const [_, age, operation_year, nb_pos_detected, surv] = queryKey;
+    return axios.get('/patients/predict/', {
+        params: {
+            age,
+            operation_year,
+            nb_pos_detected,
+            surv: surv ? 1 : 2,
+        },
+    });
+};
