d6ba0c0b1fa28c7532311d18ab58a991f13d5beb
Parkjju <rudwns3927@gmail.com>
2023-04-28 20:50:59 +0900

    feat: 차트 데이터프롭스 연동

--- src/components/MyChart.js
+++ src/components/MyChart.js
@@ -10,7 +10,7 @@ import {
     Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import { faker } from '@faker-js/faker';
+import { useSelector } from 'react-redux';
 
 ChartJS.register(
     CategoryScale,
@@ -22,60 +22,76 @@ ChartJS.register(
     Legend
 );
 
-export const options = {
-    responsive: true,
-    interaction: {
-        mode: 'index',
-        intersect: false,
-    },
-    stacked: false,
-    plugins: {
-        title: {
-            display: true,
-            text: 'Chart.js Line Chart - Multi Axis',
-        },
-    },
-    scales: {
-        y: {
-            type: 'linear',
-            display: true,
-            position: 'left',
-        },
-        y1: {
-            type: 'linear',
-            display: true,
-            position: 'right',
-            grid: {
-                drawOnChartArea: false,
+let labels = [];
+for (let index = 30; index < 80; index += 1) {
+    labels.push(index);
+}
+
+export default function MyChart() {
+    const graphData = useSelector((state) => state.graph);
+
+    labels = labels.map((data) => String(data));
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: graphData[0],
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                yAxisID: 'y',
+            },
+            {
+                label: 'Dataset 2',
+                data: graphData[1],
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                yAxisID: 'y1',
+            },
+            {
+                label: 'Dataset 3',
+                data: graphData[2],
+                borderColor: '#e67e22',
+                backgroundColor: '#f1c40f',
+                yAxisID: 'y1',
+            },
+        ],
+    };
+
+    const options = {
+        elements: {
+            point: {
+                radius: 0,
             },
         },
-    },
-};
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-export const data = {
-    labels,
-    datasets: [
-        {
-            label: 'Dataset 1',
-            data: labels.map(() =>
-                faker.datatype.number({ min: -1000, max: 1000 })
-            ),
-            borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            yAxisID: 'y',
+        responsive: true,
+        interaction: {
+            mode: 'index',
+            intersect: false,
         },
-        {
-            label: 'Dataset 2',
-            data: labels.map(() =>
-                faker.datatype.number({ min: -1000, max: 1000 })
-            ),
-            borderColor: 'rgb(53, 162, 235)',
-            backgroundColor: 'rgba(53, 162, 235, 0.5)',
-            yAxisID: 'y1',
+        stacked: false,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Chart.js Line Chart - Multi Axis',
+            },
         },
-    ],
-};
-export default function MyChart() {
+        scales: {
+            y: {
+                type: 'linear',
+                display: true,
+                position: 'left',
+            },
+            y1: {
+                type: 'linear',
+                display: true,
+                position: 'right',
+                grid: {
+                    drawOnChartArea: false,
+                },
+            },
+        },
+    };
     return <Line options={options} data={data} />;
 }
