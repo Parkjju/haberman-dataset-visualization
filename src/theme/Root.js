5396d249bd6cbdf673e4a8bbea113dd1165f49e7
Parkjju <rudwns3927@gmail.com>
2023-04-30 00:02:47 +0900

    feat: GlobalStyle 컴포넌트 삽입

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Provider } from 'react-redux';
 import store from '../store';
+import { createGlobalStyle } from 'styled-components';
 
 const queryClient = new QueryClient();
+
+const GlobalStyle = createGlobalStyle`
+
+main p img{
+    width:50%;
+    display: block;
+    margin:0 auto;
+    margin-bottom:40px;
+}
+`;
 
 export default function Root({ children }) {
     return (
         <Provider store={store}>
+            <GlobalStyle />
             <QueryClientProvider client={queryClient}>
                 {children}
             </QueryClientProvider>
